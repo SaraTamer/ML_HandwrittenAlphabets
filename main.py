769e9ce5833eb543c495c18092075724db6b6805
SaraTamer <sasooelbihery@gmail.com>
2024-12-22 20:28:01 +0200

    splitting training and validation before logistic

--- main.py
+++ main.py
@@ -408,10 +408,9 @@ def main():
     n_classes = data.iloc[:, 0].nunique()
     print("Number of unique classes:", n_classes)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
-    logisticRegression(X_train, y_train, X_test, y_test)
-
+    logisticRegression(X_train, y_train, X_test, y_test, X_val, y_val)
     #------------------------------------------
     alphabetical_test()
 
